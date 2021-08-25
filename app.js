9eb8c8095b4192db6838259a6ddbc89223816cfb
Sainath <sainathsvm@gmail.com>
2021-08-26 02:27:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var txtInput = document.querySelector("#txtInput");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#outputDiv");
+
+btnTranslate.addEventListener("click", console.log("hi"));
