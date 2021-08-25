44477ba96aca0125bc00a19be89374e8561e74ba
Sainath <sainathsvm@gmail.com>
2021-08-26 02:45:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ var txtInput = document.querySelector("#txtInput");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#outputDiv");
 
-btnTranslate.addEventListener("click", console.log("hi"));
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", eventHandler);
+
+function eventHandler(){
+ outputDiv.innerText = txtInput.value;  
+}
