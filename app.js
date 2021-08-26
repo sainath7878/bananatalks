7fb7fc4e7f5fc5dc39cb523532937b69eeef6e2f
Sainath <sainathsvm@gmail.com>
2021-08-26 22:05:00 +0530

    added js and style

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ var serverURL = "https://api.funtranslations.com/translate/minion.json";
 btnTranslate.addEventListener("click", eventHandler);
 
 function eventHandler(){
- outputDiv.innerText = txtInput.value;  
+    constructURL(serverURL + "?" + "text=" + txtInput.value);
+}
+
+function constructURL(text){
+    fetch(text).then(response => response.json()).then(json => outputDiv.innerText = json.contents.translated);
 }
